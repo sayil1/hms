eee0c2a3daf73cd6acc69eb1e48d251c3a7d08f0
sayil1 <sundaysayil4u@gmail.com>
2025-12-01 19:33:47 +0100

    make up[dates

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { resolve } from 'path';
+import { join, resolve } from 'path';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
@@ -20,10 +20,14 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.useStaticAssets(resolve('./src/public'));
-  app.setBaseViewsDir(resolve('./src/views'));
+  // app.useStaticAssets(resolve('./src/public'));
+  // app.setBaseViewsDir(resolve('./src/views'));
+  // app.setViewEngine('hbs');
+
+  app.useStaticAssets(join(__dirname, 'public'));
+  app.setBaseViewsDir(join(__dirname, 'views'));
   app.setViewEngine('hbs');
 
-  await app.listen(process.env.PORT || 2000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
